02e93cbcbb2359dcb12e6030416b2365c4445d4c
Iryna Shustava <iryna@hashicorp.com>
2021-01-25 16:21:12 -0800

    wip: add logic for checking pod annotations

--- controller/service_controller.go
+++ controller/service_controller.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	connectinject "github.com/hashicorp/consul-k8s/connect-inject"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -26,6 +27,30 @@ func (r *ServiceController) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		panic(err)
 	}
 	r.Log.Info("retrieved service from kube", "svc", svc)
+
+	// get endpoints
+	var endpoints corev1.Endpoints
+	err = r.Client.Get(context.Background(), req.NamespacedName, &endpoints)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, subset := range endpoints.Subsets {
+		for _, address := range subset.Addresses {
+			if address.TargetRef.Kind == "Pod" {
+				var pod corev1.Pod
+				objectKey := types.NamespacedName{Name: address.TargetRef.Name, Namespace: address.TargetRef.Namespace}
+				err = r.Client.Get(context.Background(), objectKey, &pod)
+				if err != nil {
+					panic(err)
+				}
+
+				if _, ok := pod.ObjectMeta.Annotations[connectinject.AnnotationInject]; ok {
+					r.Log.Info("found service with connect pod annotations", "service", req.NamespacedName, "pod", pod.Name)
+				}
+			}
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
